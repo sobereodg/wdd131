573136bc23c7e3e784c11db179eb1ec5c81c831e
sobereodg <soberegoodluck8@gmail.com>
2025-03-13 23:35:41 +0100

    ass

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+console.log("Current Year:", currentYear);
+
+const lastModifiedDate = document.lastModified;
+console.log("Last Modified:", lastModifiedDate);
