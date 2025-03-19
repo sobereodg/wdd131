3b34e266a779f5ac57614255e09a82c124bbad5e
sobereodg <gsobereetete@byupathway.edu>
2025-03-19 20:21:32 +0100

    boooo

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -13,3 +13,36 @@ deleteButton.textContent = '❌';
 li.append(deleteButton);
 
 list.append(li);
+
+buttton.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('list');
+
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+    } else {
+        alert('Please enter a chapter name.');
+        input.focus();
+    }
+});
