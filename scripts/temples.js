ccb61144730867d1da3922c6bb5b1bdfa521c654
sobereodg <gsobereetete@byupathway.edu>
2025-03-27 13:19:20 +0100

    watt

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,11 @@
-// Select elements
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
-// Toggle mobile menu
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("show");
     menuButton.classList.toggle("show");
 });
 
-// Display current year in footer
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
 
-// Display last modified date
 document.querySelector("#lastModified").textContent = "Last Modified: " + document.lastModified;
